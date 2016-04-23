d8db46a36c3f55d609c1fb844401ba981a9396fc
André Durão <andreluisdurao@gmail.com>
2016-04-23 17:44:50 -0300

    Adding older c code from bitbucket repo on the gem repo TO BE migrated

--- ext/trie/trie.c
+++ ext/trie/trie.c
@@ -116,3 +116,130 @@ Init_trie(void) {
     rb_define_method(cTrie, "initialize", trie_init, 1);
     rb_define_method(cTrie, "free", trie_release, 0);
 }
+
+/*
+ * Older functions from the original trie C repo (based on #TODO: find original github repo)
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <assert.h>
+
+ #include "trie.h"
+
+ inline trie_t *trie_init(void) {
+     trie_t *t = (trie_t *) malloc(sizeof(trie_t));
+     t->node = 0;
+     return t;
+ }
+
+ void trie_add(trie_t *t, char *word) {
+     int c;
+     while ((c = *word++)) {
+         assert(c >= 97 && c < 123);
+         if (t->chars[c-TRIE_OFFSET] == NULL) {
+             t->chars[c-TRIE_OFFSET] = trie_init();
+         }
+         t->node = 1;
+         t = t->chars[c-TRIE_OFFSET];
+     }
+     t->node = 1;
+     t->chars[TRIE_SENTINEL] = trie_init();
+ }
+
+ int trie_exists(trie_t *t, char *word) {
+     int c;
+     while ((c = *word++)) {
+         if (t->chars[c-TRIE_OFFSET] == NULL) {
+             return 0;
+         }
+         t = t->chars[c-TRIE_OFFSET];
+     }
+     return t->chars[TRIE_SENTINEL] != NULL ? 1 : 0;
+ }
+
+ int trie_prefix(trie_t *t, char *prefix) {
+     int c;
+     while ((c = *prefix++)) {
+         if (t->chars[c-TRIE_OFFSET] == NULL) {
+             return 0;
+         }
+         t = t->chars[c-TRIE_OFFSET];
+     }
+     return t->node;
+ }
+
+ int trie_load(trie_t *t, char *file) {
+     FILE *stream = fopen(file, "r");
+     if (stream == NULL) {
+         return -1;
+     }
+
+     trie_t *root = t;
+     int c, words = 0, word_len = 0;
+     while ((c = getc(stream)) != EOF) {
+         if (c == '\n' || c == '\r') {
+             if (word_len > 0) {
+                 t->chars[TRIE_SENTINEL] = trie_init();
+                 words++;
+                 word_len = 0;
+                 t = root;
+             }
+         } else {
+             word_len++;
+             assert(c >= 97 && c < 123);
+             if (t->chars[c-TRIE_OFFSET] == NULL) {
+                 t->chars[c-TRIE_OFFSET] = trie_init();
+             }
+             t->node = 1;
+             t = t->chars[c-TRIE_OFFSET];
+         }
+     }
+     if (t != root && word_len > 0) {
+         t->chars[TRIE_SENTINEL] = trie_init();
+     }
+     return words;
+ }
+
+ void trie_strip(trie_t *t, char *src, char *dest) {
+     if (src == NULL) {
+         return;
+     }
+     if (dest == NULL) {
+         dest = src;
+     }
+     int c, i = 0, last_break = 0, in_trie = 1;
+     trie_t *root = t;
+
+     while ((c = dest[i++] = *src++)) {
+         if (c == ' ' || c == '\n' || c == '\t' || c == '\r') {
+             t = root;
+             if (in_trie) {
+                 i = last_break;
+             } else {
+                 in_trie = 1;
+                 last_break = i;
+             }
+             continue;
+         }
+         if (!in_trie) {
+             continue;
+         }
+         if (t->chars[c-TRIE_OFFSET] == NULL) {
+             in_trie = 0;
+         } else {
+             t = t->chars[c-TRIE_OFFSET];
+             in_trie = 1;
+         }
+     }
+ }
+
+ void trie_free(trie_t *t) {
+   int i;
+     for (i = 0; i < TRIE_SIZE; i++) {
+         if (t->chars[i] != NULL) {
+             trie_free(t->chars[i]);
+         }
+     }
+     free(t);
+ }
